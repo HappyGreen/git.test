d8777f01c475db0214148db2ba4da1003dc09f89
GreenSnake <snake.c2h5oh@gmail.com>
2016-10-26 17:00:21 +0500

    1st file add

---
+++ systest.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*-: coding: utf-8 -*-
+
+import sys
+
+print(sys.argv)
+print('Version:\t{}\nVers info:\t{}\nPlatform:\t{}\nWinver:\t'.format(sys.version, sys.version_info, sys.platform, sys.winver))
